5d1af9c6059dc52f18525721c06c02dd71ff8389
yusuke munakata <yusuke.munakata0830@gmail.com>
2026-03-30 21:28:58 +0900

    Initialize Spring Boot project

---
+++ src/main/kotlin/com/example/ReserveLite/ReserveLiteApplication.kt
@@ -0,0 +1,11 @@
+package com.example.ReserveLite
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ReserveLiteApplication
+
+fun main(args: Array<String>) {
+	runApplication<ReserveLiteApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/ReserveLite/ReserveLiteApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.ReserveLite
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ReserveLiteApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
