87ad10ec4c4b89b3b7ae021006e671a4ba0adc5e
yusuke munakata <yusuke.munakata0830@gmail.com>
2026-04-01 22:02:28 +0900

    feat: CustomerCreateFormを作成

---
+++ src/main/kotlin/com/example/ReserveLite/form/CustomerCreateForm.kt
@@ -0,0 +1,16 @@
+package com.example.ReserveLite.form
+
+import jakarta.validation.constraints.Email
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Size
+
+data class CustomerCreateForm(
+    @field:NotBlank(message = "名前は必須項目です。")
+    @field:Size(max = 100, message = "名前は100文字以内で入力してください。")
+    val name: String,
+
+    @field:NotBlank(message = "メールアドレスは必須項目です。")
+    @field:Email(message = "メールアドレスの形式が正しくありません。")
+    @field:Size(max = 255, message = "メールアドレスは255文字以内で入力してください。")
+    val email: String
+)
